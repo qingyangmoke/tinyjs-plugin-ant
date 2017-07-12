84f4947b7c770179bd13446aff6b9451d2de86ef
采东 <fusheng.sfs@antfin.com>
2017-07-12 15:41:19 +0800

    改成es5

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
 const full = require('./config/webpack.full.config');
 const p2 = require('./config/webpack.p2.config');
 const ant = require('./config/webpack.ant.config');
-module.exports = [...full, ...p2, ...ant];
+module.exports = [full[0], full[1], p2[0], p2[1], ant[0], ant[1]];
